782edd92a0e1fff4b599ea8f7ca14be12cdec104
rashadwhy <anwar.muradin@gmail.com>
2023-04-19 14:00:45 +0200

    Import cleanup

--- ZiggoFlexProgram/src/main/InteractiveMenu.java
+++ ZiggoFlexProgram/src/main/InteractiveMenu.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.util.List;
+
 class InteractiveMenu {
     private final Scanner scanner = new Scanner(System.in);
     public final KlantDatabase database = new KlantDatabase();
